0b2517c5f7216aaa4eb5dfab5144b4e2f219df0c
Codingexperthub Pvt Ld <114645157+CodingExpertHub@users.noreply.github.com>
2024-03-06 12:43:48 +0530

    Update Data.jsx

--- src/Data.jsx
+++ src/Data.jsx
@@ -23,13 +23,13 @@ import logo8 from "./assets/logo8.png";
 
 import pic1 from "./assets/pic1.png";
 import pic2 from "./assets/pic2.png";
-import pic3 from "./assets/pic3.png";
+import pic3 from "./assets/pic9.png";
 import pic4 from "./assets/pic4.png";
 import pic5 from "./assets/pic5.png";
 import pic6 from "./assets/pic6.png";
 import pic7 from "./assets/pic7.png";
 import pic8 from "./assets/pic8.png";
-import pic9 from "./assets/pic9.png";
+import pic9 from "./assets/pic3.png";
 import pic10 from "./assets/pic10.png";
 
 
